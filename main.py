3b5883f36420480bb45c710509d343e0b34567f7
lucas-santosr <lsr1304@hotmail.com>
2025-01-05 17:08:12 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornada de dados")
+
+# Variáveis
+
+idade = 30
+nome = "Lucas"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+list_invertida = lista.reverse()
